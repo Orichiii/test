8db577414fe05ecf7b40b88cb01314ce3dcfcb00
Orichiii <38610952+Orichiii@users.noreply.github.com>
2026-06-02 09:47:56 +0800

    Initial commit

---
+++ test.py
@@ -0,0 +1,10 @@
+print('hello world');
+
+import numpy as np  # 导入 numpy 库用于数组计算
+arr = np.array([1, 2, 3, 4, 5])  # 创建一个 NumPy 数组
+print(arr)  # 输出数组内容
+
+# 打印当前日期时间
+import datetime
+now = datetime.datetime.now()
+print(now)
